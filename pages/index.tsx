d288b10838089d440f00296addd3ba48502a712e
holomdev <holom.dev@gmail.com>
2023-03-01 17:11:56 -0600

    mostrar tarjetas de pokemon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,58 @@
 import { GetStaticProps, NextPage } from 'next';
 import { Layout } from '@/components/layouts'
 import { pokeApi } from '@/api';
-import { PokemonListResponse } from '@/interfaces';
+import { PokemonListResponse, SmallPokemon } from '@/interfaces';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
 
-const HomePage: NextPage = (props) => {
-  console.log(props)
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
-    <Layout title="Listado de pokemons">
-      <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-      </ul>
+    <Layout title='Listado de pokemons'>
+
+      <Grid.Container gap={2} justify='flex-start'>
+        {
+          pokemons.map(({id, name, img}) => (
+            <Grid key={id} xs={6} sm={3} md={2} xl={ 1 }>
+              <Card isHoverable isPressable>
+                <Card.Body css={{ p: 1 }}>
+                  <Card.Image 
+                    src={img} 
+                    width='100%' 
+                    height={140}
+                  />
+                </Card.Body>
+                <Card.Footer>
+                  <Row justify="space-between">
+                    <Text transform='capitalize'>{name}</Text>
+                    <Text>#{id}</Text>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid.Container>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+
+  const pokemons: SmallPokemon[] = data.results.map((poke, i) => ({
+    ...poke,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
+  }));
+
   return {
     props: {
-      pokemons: data.results
+      pokemons
     }
   }
 }
+
 export default HomePage
